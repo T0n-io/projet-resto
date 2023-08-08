93cda5cd7c91c8e9261201a4d9fa17829a7bc786
Ton.io <ton.io-webagence@gmail.com>
2023-08-08 16:01:08 +0200

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function LoginPage() {
-  return <h1>Login</h1>;
+  //state
+  const [inputValue, setInputValue] = useState("")
+  //comportement
+const handleSubmit = (event) => {
+  event.preventDefault() 
+  alert(`Bonjour ${inputValue}`)
+  setInputValue("")
+ }
+ const handleChange = (event) => { 
+  setInputValue(event.target.value)
+  }
+  // affichage
+  return (
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+          <input value={inputValue} onChange={handleChange} type="text" placeholder="Entrez votre prénom" required />
+          <button>Accédez à votre espace</button>
+      </form>
+    </div>
+  );
 }
