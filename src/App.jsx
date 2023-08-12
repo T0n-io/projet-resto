cbd65c38fdc3f4f6030e71a6cc2b87a5962cbefb
Ton.io <ton.io-webagence@gmail.com>
2023-08-12 20:41:53 +0200

    ✨ F02: APP > install routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router";
 import LoginPage from "./components/pages/login/LoginPage";
+import { Link } from "react-router-dom";
+import OrderPage from "./components/pages/order/OrderPage";
+import ErrorPage from "./components/pages/Error/ErrorPage";
 
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 // import "./App.css";
 
 function App() {
- return <LoginPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/order" element={<OrderPage />} />
+      <Route path="/*" element={<ErrorPage />} />
+    </Routes>
+  );
 }
 
 export default App;
