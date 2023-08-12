0fcd00199ea53bc2b36cdf0aabbbed97a7cc083b
Ton.io <ton.io-webagence@gmail.com>
2023-08-12 20:52:19 +0200

    ✨ F02: ORDER > add redirection from/to OrderPage

--- src/components/pages/Order/OrderPage.jsx
+++ src/components/pages/Order/OrderPage.jsx
@@ -1,5 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function OrderPage() {
   return (
-    <div>OrderPage</div>
+    <Link to="/">
+      <div>OrderPage</div>
+    </Link>
   )
 }
