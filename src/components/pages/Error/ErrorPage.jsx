cdcd72de2cb882e05a19e9f6685242aa8dd84839
Ton.io <ton.io-webagence@gmail.com>
2023-08-12 20:56:51 +0200

    ✨ F02: ORDER > add redirection from/to ErrorPage

--- src/components/pages/Error/ErrorPage.jsx
+++ src/components/pages/Error/ErrorPage.jsx
@@ -1,5 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
-  )
+    <Link to="/">
+      <button>Retourner à la page d'accueil</button>
+    </Link>
+  );
 }
