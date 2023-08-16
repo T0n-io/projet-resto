e3b2197bc4ab15606b236c408c8514245b0fc7b1
Ton.io <ton.io-webagence@gmail.com>
2023-08-16 14:30:03 +0200

    💄 F03: LOGIN > LoginForm > button > add  styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router";
 import styled from "styled-components";
 import { theme } from "../../..";
 import { BsPersonCircle } from "react-icons/bs";
+import { IoChevronForward } from "react-icons/io5";
 
 export default function LoginForm() {
   //state
@@ -26,7 +27,7 @@ export default function LoginForm() {
         <hr />
         <h1>Connectez vous</h1>
         <div className="input-container">
-          <BsPersonCircle className="icon-person"/>
+          <BsPersonCircle className="icon" />
           <input
             value={inputValue}
             onChange={handleChange}
@@ -35,7 +36,10 @@ export default function LoginForm() {
             required
           />
         </div>
-          <button>Accédez à mon espace</button>
+        <button className="button-container">
+          <span>Accédez à mon espace</span>
+          <IoChevronForward className="icon"/>
+        </button>
       </LoginFormStyled>
     </>
   );
@@ -70,7 +74,7 @@ const LoginFormStyled = styled.form`
     padding: 18px 24px;
     margin: 18px 0;
     background-color: ${theme.colors.white};
-    .icon-person {
+    .icon {
       color: ${theme.colors.greyMedium};
       font-size: 15px;
       margin-right: 8px;
@@ -80,5 +84,61 @@ const LoginFormStyled = styled.form`
       text-align: left;
       width: 100%;
     }
+    ::placeholder {
+      color: ${theme.colors.greyMedium};
+
   }
+}
+  .button-container {
+    width: 100%;
+    font-size: 15px;
+    font-weight: 800;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    position: relative;
+    white-space: nowrap;
+    text-decoration: none;
+    line-height: 1;
+    border: none;
+
+    padding: 18px 24px;
+    border-radius: 5px;
+    background-color: ${theme.colors.primary_burger};
+    color: ${theme.colors.white};
+
+    .icon{
+      font-size: 15px;
+      margin-left: 10px;
+      margin-top: 3px;
+    }
+   
+    
+    &:hover:not(disabled) {
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.primary_burger};
+      border: solid 1.5px ${theme.colors.primary_burger};
+      transition: all 0.2s ease-out;
+    }
+    &:active{
+      color: ${theme.colors.white};
+      background-color: ${theme.colors.primary_burger};
+      border: solid 1.5px ${theme.colors.primary_burger};
+    }
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }    
+    button{
+      background-color: ${theme.colors.primary_burger};
+      font-size: 15px;
+      font-weight: 800;
+      cursor: pointer;
+      width: 100%;
+      height: 100%;
+/*       
+
+      }
+    }
+    }
 `;
