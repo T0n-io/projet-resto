8dcd1eb732fab6f529182eb7d228551433483ed4
Ton.io <ton.io-webagence@gmail.com>
2023-08-16 13:26:03 +0200

    💄 F03: LOGO > Add Styling

--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -3,25 +3,31 @@ import { theme } from "../..";
 export default function Logo() {
   return (
     <LogoStyled>
-      {/* <h3>CRAZEE</h3>
-      <img src="../../../src/assets/images/F03-logo-orange.png" alt="Logo" />;
-      <h3>BURGER</h3> */}
-      LOGO CRAZEE BURGER
+      <h1>CRAZEE</h1>
+      <img src="/images/F03-logo-orange.png" alt="Logo Crazee Burger" />;<h1>BURGER</h1>
     </LogoStyled>
   );
 }
 
 const LogoStyled = styled.div`
-background: blue;
+  display: flex;
+  align-items: center;
+  transform: scale(2.5);
 
-  /*
   img {
-    width: 200px;
-    height: 150px;
+    width: 80px;
+    height: 60px;
+    object-fit: contain;
+    object-position: center;
+    margin: 0 5px;
   }
-  h3 {
+  h1 {
     color: ${theme.colors.primary_burger};
-    font-size: 110px;
+    display: inline;
+    font-size: 36px;
+    line-height: 1em;
+    font-weight: 700;
+    letter-spacing: 1.5px;
     font-family: "Amatic SC", cursive;
-  } */
+  }
 `;
