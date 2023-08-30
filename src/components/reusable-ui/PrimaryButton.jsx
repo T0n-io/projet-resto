b7669d113980a8bd1bbfc5d7c46dd61d76403383
Ton.io <ton.io-webagence@gmail.com>
2023-08-30 16:07:03 +0200

    🎨 F07: Upgrade Primarybutton

--- src/components/reusable-ui/PrimaryButton.jsx
+++ src/components/reusable-ui/PrimaryButton.jsx
@@ -1,54 +1,63 @@
-import { styled } from "styled-components";
-import { theme } from "../..";
+import styled from "styled-components"
+import { theme } from "../.."
 
-export default function PrimaryButton({ label, Icon }) {
+export default function PrimaryButton({ label, Icon, className }) {
   return (
-    <PrimaryButtonStyled>
+    <PrimaryButtonStyled className={className}>
       <span>{label}</span>
       {Icon && Icon}
     </PrimaryButtonStyled>
-  );
+  )
 }
 
 const PrimaryButtonStyled = styled.button`
- 
   width: 100%;
-  font-size: 15px;
-  font-weight: ${theme.fonts.weights.heavy};
-  display: flex;
+  border: 1px solid red;
+  display: inline-flex;
   justify-content: center;
   align-items: center;
-  position: relative;
-  white-space: nowrap;
-  text-decoration: none;
+  position: relative; //is used in case you want to create interactive icons where an icon replaces the text label.
+  white-space: nowrap; //prevents the text label from wrapping to the next line.
+  text-decoration: none; //removes the text decoration in case you’re applying the .btn class to a link.
   line-height: 1;
-  border: none;
+
   padding: 18px 24px;
   border-radius: 5px;
-  background-color: ${theme.colors.primary_burger};
-  color: ${theme.colors.white};
+  font-size: 15px;
+  font-weight: 800;
+  color: white;
+  background-color: #ff9f1b;
+  border: 1px solid #ff9f1b;
 
-  &:hover:not(disabled) {
+  :hover {
+    color: ${theme.colors.primary};
     background-color: ${theme.colors.white};
-    color: ${theme.colors.primary_burger};
-    border: solid 1.5px ${theme.colors.primary_burger};
-    transition: all 0.2s ease-out;
+    border: 1px solid ${theme.colors.primary};
+    transition: all 200ms ease-out;
   }
-  &:active {
+  :active {
+    background-color: ${theme.colors.primary};
     color: ${theme.colors.white};
-    background-color: ${theme.colors.primary_burger};
-    border: solid 1.5px ${theme.colors.primary_burger};
   }
-  &:disabled {
-    opacity: 0.6;
+
+  &.is-disabled {
+    opacity: 50%;
     cursor: not-allowed;
+    z-index: 2;
   }
-  /* button {
-    background-color: ${theme.colors.primary_burger};
-    font-size: 15px;
-    font-weight: 800;
-    cursor: pointer;
-    width: 100%;
-    height: 100%;
-  } */
-`;
+
+  &.with-focus {
+    border: 1px solid white;
+    background-color: ${theme.colors.white};
+    color: ${theme.colors.primary};
+    :hover {
+      color: ${theme.colors.white};
+      background-color: ${theme.colors.primary};
+      border: 1px solid ${theme.colors.white};
+    }
+    :active {
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.primary};
+    }
+  }
+`
