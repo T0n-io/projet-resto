a42e7952cef4c0e74f6a9e8816899c3ee247d944
Ton.io <ton.io-webagence@gmail.com>
2023-09-12 18:50:49 +0200

    🐛 F08: AdminPanel > add box-sizing

--- src/components/pages/Order/Main/MainRightSide/AdminPanel/AdminPanel.jsx
+++ src/components/pages/Order/Main/MainRightSide/AdminPanel/AdminPanel.jsx
@@ -18,9 +18,10 @@ export default function AdminPanel() {
 }
 
 const AdminPanelStyled = styled.div`
-  height: 250px;
+  height: 240px;
   background: ${theme.colors.white};
   border: 1px solid ${theme.colors.greyLight};
   box-shadow: ${theme.shadows.subtle};
+  box-sizing: border-box;
   padding: 30px 5%;
 `
