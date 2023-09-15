7fc7b371d3442b577383d1da68c06b580918d4fc
Ton.io <ton.io-webagence@gmail.com>
2023-09-15 12:51:10 +0200

    💄 F10: BASETK > add box shadow

--- src/components/pages/Order/Main/Basket.jsx
+++ src/components/pages/Order/Main/Basket.jsx
@@ -16,18 +16,19 @@ const BasketStyled = styled.div`
   /* background-color: pink; */
   display: flex;
   flex-direction: column;
-
-
+  
+  
   .head {
     height: 70px;
     background-color: ${theme.colors.background_dark};
     /* border-top-left-radius: ${theme.borderRadius.extraRound}; */
-
+    
   }
   .body{
     flex: 1;
     background-color: ${theme.colors.background_white};
     height: 100%;
+    box-shadow: ${theme.shadows.basket};
   }
   .footer{
     height: 70px;
