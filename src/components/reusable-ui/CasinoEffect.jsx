a7c1e112e0f8904816b6bbdcf614de4e73ca387b
Ton.io <ton.io-webagence@gmail.com>
2023-09-20 17:05:52 +0200

    🚧  F14: BASKET > Total > Make both elements visible on props change

--- src/components/reusable-ui/CasinoEffect.jsx
+++ src/components/reusable-ui/CasinoEffect.jsx
@@ -1,5 +1,43 @@
+/* eslint-disable react/prop-types */
 
-export default function CasinoEffect({count, className}) {
-  return <span className={className}>{count}</span>
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { styled } from "styled-components";
+
+export default function CasinoEffect({ count, className }) {
+  return (
+  <TransitionGroup component={CasinoEffectStyled} className="transition-group">
+    <CSSTransition classNames={"count-animated"} timeout={500} key={count}>
+      <span className={className}>{count}</span>
+    </CSSTransition>
+  </TransitionGroup>
     
+  )
 }
+
+const CasinoEffectStyled = styled.div`
+  border: 1px solid red;
+
+  span {
+    display: inline-block;
+  }
+  /* Mouting animation */
+  .count-animated-enter {
+    transform: translateY(100%);
+  }
+  .count-animated-enter-active {
+    transform: translateY(0%);
+    transition: 2s;
+  }
+  .count-animated-enter-done {
+  }
+
+  /* Unmouting animation */
+  .count-animated-exit {
+    transform: translateY(100%);
+  }
+  .count-animated-exit-active {
+    transform: translateY(0%);
+    transition: 2s;
+  }
+
+`;
