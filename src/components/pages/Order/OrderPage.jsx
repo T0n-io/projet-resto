d56997a374d0dc5a57472727590788f9b9b9bd2d
Ton.io <ton.io-webagence@gmail.com>
2023-09-20 10:39:50 +0200

    ♻️  F13: BASKET > Firestore > extract logique into helper initialiseUserSession

--- src/components/pages/Order/OrderPage.jsx
+++ src/components/pages/Order/OrderPage.jsx
@@ -10,95 +10,93 @@ import { useMenu } from "../../../hooks/useMenu";
 import { useBasket } from "../../../hooks/useBasket";
 import { findObjectById } from "../../../utils/array";
 import { useParams } from "react-router-dom";
-import { getMenu } from "../../../api/product";
-import { getLocalStorage } from "../../../utils/window";
+import { initialiseUserSession } from "./helpers/initialiseUserSession";
 
 export default function OrderPage() {
- // state
- const [isModeAdmin, setIsModeAdmin] = useState(true)
- const [isCollapsed, setIsCollapsed] = useState(false)
- const [currentTabSelected, setCurrentTabSelected] = useState("edit")
- const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
- const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
- const titleEditRef = useRef();
-const {menu, setMenu, handleAdd, handleDelete, resetMenu, handleEdit} = useMenu()
-const {basket, setBasket, handleAddToBasket, handleDeleteBasketProduct} = useBasket()
+  // state
+  const [isModeAdmin, setIsModeAdmin] = useState(true);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [currentTabSelected, setCurrentTabSelected] = useState("edit");
+  const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
+  const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
+  const titleEditRef = useRef();
+  const { menu, setMenu, handleAdd, handleDelete, resetMenu, handleEdit } =
+    useMenu();
+  const { basket, setBasket, handleAddToBasket, handleDeleteBasketProduct } =
+    useBasket();
   const { username } = useParams();
 
+  const handleProductSelected = async (idProductClicked) => {
+    const productClickedOn = findObjectById(idProductClicked, menu);
+    await setIsCollapsed(false);
+    await setCurrentTabSelected("edit");
+    await setProductSelected(productClickedOn);
+    titleEditRef.current.focus();
+  };
+  initialiseUserSession(username, setMenu, setBasket);
 
-const handleProductSelected = async (idProductClicked) => { 
-  const productClickedOn = findObjectById(idProductClicked, menu);
-  await setIsCollapsed(false);
-  await setCurrentTabSelected("edit");
-  await setProductSelected(productClickedOn);
-  titleEditRef.current.focus();
+  useEffect(() => {
+    initialiseUserSession(username, setMenu, setBasket);
+  }, []);
 
- }
+  //  const initialiseMenu = async () =>  {
+  //   const menuReceived = await getMenu(username)
+  //   setMenu(menuReceived)
+  //  }
+  //  const initialiseBasket = () =>  {
+  //   const basketReceived = getLocalStorage(username) // localstorage est synchrone, pas besoin d'async/await
+  //   if(basketReceived) setBasket(basketReceived)
+  //  }
+  // const initialiseUserSession = async () => {
+  //   await initialiseMenu()
+  //   initialiseBasket()
+  // }
+  //  // eslint-disable-next-line react-hooks/exhaustive-deps
+  //  useEffect(() => {
+  //   initialiseUserSession()
+  //  }, [])
 
- const initialiseMenu = async () =>  {
-  const menuReceived = await getMenu(username)
-  setMenu(menuReceived)
- }
- const initialiseBasket = () =>  {
-  const basketReceived = getLocalStorage(username) // localstorage est synchrone, pas besoin d'async/await
-  if(basketReceived) setBasket(basketReceived)
- }
-const initialiseUserSession = async () => {
-  await initialiseMenu()
-  initialiseBasket()
-}
- // eslint-disable-next-line react-hooks/exhaustive-deps
- useEffect(() => {
-  initialiseUserSession()
- }, [])
- useEffect(() => {
- // eslint-disable-next-line react-hooks/exhaustive-deps
- }, [])
- 
- //CRUD
+  //CRUD
 
-const orderContextValue = {
-  username,
-  isModeAdmin,
-  setIsModeAdmin,
-  isCollapsed,
-  setIsCollapsed,
-  currentTabSelected,
-  setCurrentTabSelected,
-  handleAdd,
-  menu,
-  setMenu,
-  handleDelete,
-  resetMenu,
-  newProduct,
-  setNewProduct,
-  productSelected,
-  setProductSelected,
-  handleEdit,
-  titleEditRef,
-  basket,
-  handleAddToBasket,
-  handleDeleteBasketProduct,
-  handleProductSelected,
+  const orderContextValue = {
+    username,
+    isModeAdmin,
+    setIsModeAdmin,
+    isCollapsed,
+    setIsCollapsed,
+    currentTabSelected,
+    setCurrentTabSelected,
+    handleAdd,
+    menu,
+    setMenu,
+    handleDelete,
+    resetMenu,
+    newProduct,
+    setNewProduct,
+    productSelected,
+    setProductSelected,
+    handleEdit,
+    titleEditRef,
+    basket,
+    handleAddToBasket,
+    handleDeleteBasketProduct,
+    handleProductSelected,
+  };
 
-}
+  // comportements
 
-
- // comportements
-
- 
   return (
     // <AdminModeProvider>
     //   <PanelProvider>
-    <OrderContext.Provider value={orderContextValue} >
-        <OrderPageStyled>
-          <div className="container">
-            <NavBar />
-            <Main />
-          </div>
-        </OrderPageStyled>
-    {/* //   </PanelProvider> */}
-    {/* // </AdminModeProvider> */}
+    <OrderContext.Provider value={orderContextValue}>
+      <OrderPageStyled>
+        <div className="container">
+          <NavBar />
+          <Main />
+        </div>
+      </OrderPageStyled>
+      {/* //   </PanelProvider> */}
+      {/* // </AdminModeProvider> */}
     </OrderContext.Provider>
   );
 }
