7ee7bd7c88693be54f98bea36063a1ed4ecacd4a
Ton.io <ton.io-webagence@gmail.com>
2023-09-21 10:31:25 +0200

    🐛 B01: LOGIN > Require userReceived's username to redirect user to OrderPage

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -17,9 +17,9 @@ export default function LoginForm() {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    authenticateUser(username);
+    const userReceived = await authenticateUser(username);
     setUsername("");
-    navigate(`order/${username}`);
+    navigate(`order/${userReceived.username}`);
   };
   const handleChange = (event) => {
     setUsername(event.target.value);
