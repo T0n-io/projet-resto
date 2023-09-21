64f95bb7d5d92c7d407af267cc7dcc3a5f1ae179
Ton.io <ton.io-webagence@gmail.com>
2023-09-21 14:46:51 +0200

    ✨ F15: PUBLICITY > MENU > Card > Add new badge when product isPublicised

--- src/components/pages/Order/Main/Basket/BasketBody/BasketProducts.jsx
+++ src/components/pages/Order/Main/Basket/BasketBody/BasketProducts.jsx
@@ -9,6 +9,7 @@ import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { basketAnimation } from "../../../../../../theme/animations";
 import { formatPrice } from "../../../../../../utils/maths";
 import { convertStringToBoolean } from "../../../../../../utils/string";
+import Sticker from "../../../../../reusable-ui/Sticker.jsx";
 
 export default function BasketProducts() {
   const {
@@ -39,6 +40,7 @@ export default function BasketProducts() {
               timeout={500}
             >
               <div className="card-container">
+                {convertStringToBoolean(menuProduct.isPublicised) && <Sticker className="badge-new"/>}
                 <BasketCard
                   {...menuProduct}
                   imageSource={
@@ -71,17 +73,27 @@ const BasketProductsStyled = styled.div`
   display: flex;
   flex-direction: column;
   overflow-y: scroll;
+
   .card-container {
     margin: 10px 16px;
-    /* height: 86%; */
+    height: 86px;
     box-sizing: border-box;
+    position: relative;
     &:first-child {
       margin-top: 20px;
     }
     &:last-child {
       margin-bottom: 20px;
     }
- 
+    .badge-new {
+      position: absolute;
+      z-index: 1;
+      bottom: 10%;
+      left: 21%;
+      transform: translateY(-21%);
+      transform: translateX(-5%);
+      transform: scale(1.5);
+    }
   }
   ${basketAnimation}
 `;
